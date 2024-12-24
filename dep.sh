d2840d0445abad92aaaac3eeddb9630c6946eec7
Robin Müller <robin-m-99@gmx.de>
2024-12-24 10:54:11 +0100

    Update apt when installing dependencies

--- dep.sh
+++ dep.sh
@@ -14,6 +14,7 @@ if command -v rosdep &> /dev/null; then
     if [ ! -d /etc/ros/rosdep/sources.list.d ]; then
         sudo rosdep init -q # Init rosdep if not happend yet
     fi
+    sudo apt update
     rosdep update
     rosdep install --from-paths "./src" --ignore-src -y
 fi
